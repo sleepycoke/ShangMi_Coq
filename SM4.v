39086ba36d57b0609260aa4e4e128ece1d057168
sleepycoke <sleepycoke@users.noreply.github.com>
2019-05-13 19:30:53 +0800

    Before uglifying SM4.

--- SM4.v
+++ SM4.v
@@ -22,45 +22,7 @@ Definition T' (A : N) : N := L' (Tau A).
 
 Definition F (A : N) (B : N) (C : N) (D : N) (E : N) : N :=
   A $ (T (B $ C $ D $ E)). 
-
-
-(* ill-formed
-Fixpoint X(i : N)(X0 : N)(X1 : N)(X2 : N)(X3 : N) {struct i} : N :=
-  if eqb i 0 then X0 else
-  if eqb i 1 then X1 else
-  if eqb i 2 then X2 else
-  if eqb i 3 then X3 else
-  F (X (i - 4) X0 X1 X2 X3) (X (i - 3) X0 X1 X2 X3) (X (i - 2) X0 X1 X2 X3) (X (i - 1) X0 X1 X2 X3) (rk (i - 4)). 
-  *)
-(* expected i as N
-Fixpoint X(i : nat)(X0 : N)(X1 : N)(X2 : N)(X3 : N) {struct i} : N :=
-  if eqb i 0 then X0 else
-  if eqb i 1 then X1 else
-  if eqb i 2 then X2 else
-  if eqb i 3 then X3 else
-  F (X (i - 4) X0 X1 X2 X3) (X (i - 3) X0 X1 X2 X3) (X (i - 2) X0 X1 X2 X3) (X (i - 1) X0 X1 X2 X3) (rk (i - 4)). 
-*)
-Fixpoint X(i : nat)(X0 : N)(X1 : N)(X2 : N)(X3 : N)(rk : nat -> N)  {struct i} : N :=
-  match i with
-  | O => X0
-  | S i' =>
-      match i' with
-      | O => X1
-      | S i'' =>
-          match i'' with
-          | O => X2
-          | S i''' => 
-              match i''' with
-              | O => X3
-              | S i'''' =>
-                F (X (i'''') X0 X1 X2 X3 rk) (X (i''') X0 X1 X2 X3 rk)
-                  (X (i'') X0 X1 X2 X3 rk) (X (i') X0 X1 X2 X3 rk) (rk i'''')
-              end
-          end
-      end
-  end.
-
-Definition X_vec_axl (vec : @quadruple N) (i : nat) (rk : nat -> N) : @quadruple N :=
+Definition X_vec_aux (vec : @quadruple N) (i : nat) (rk : nat -> N) : @quadruple N :=
       (
          q2nd vec, 
          q3rd vec, 
@@ -73,46 +35,16 @@ Definition X_vec_axl (vec : @quadruple N) (i : nat) (rk : nat -> N) : @quadruple
 Fixpoint X_vec (i : nat)(X_vec0 : @quadruple N)(rk : nat -> N) : @quadruple N :=
   match i with
   | O => X_vec0
-  | S i' => X_vec_axl (X_vec i' X_vec0 rk) i' rk 
+  | S i' => X_vec_aux (X_vec i' X_vec0 rk) i' rk 
   end. 
 
-Definition calXi_plus3 (i : nat)(X0 : N)(X1 : N)(X2 : N)(X3 : N)(rk : nat->N) : N := 
-  (shiftl (X (i + 3) X0 X1 X2 X3 rk) (word_size * 3)) +   
-  (shiftl (X (i + 2) X0 X1 X2 X3 rk) (word_size * 2)) +   
-  (shiftl (X (i + 1) X0 X1 X2 X3 rk) word_size ) +   
-  (X i X0 X1 X2 X3 rk). 
 Definition SM4_enc (i : nat)(x : N) (rk : nat->N) : N :=
-  calXi_plus3 i (mask_14 x) (mask_24 x) (mask_34 x) (mask_44 x) rk. 
-
-Definition SM4_enc_fast (i : nat)(x : N) (rk : nat->N) : N :=
   R (quad2N (X_vec i (N2quad x) rk)). 
 
 Definition SM4_dec (i : nat)(y : N)(rk : nat -> N) : N :=
   SM4_enc i y (fun (j : nat) => rk(Nat.sub 31 j)). 
-  
-Definition SM4_dec_fast (i : nat)(y : N)(rk : nat -> N) : N :=
-  SM4_enc_fast i y (fun (j : nat) => rk(Nat.sub 31 j)). 
 
-Fixpoint K (i : nat) (MK : N) : N := 
-  match i with
-  | O => (mask_14 MK) $ (FK 0)
-  | S i' =>
-      match i' with
-      | O => (mask_24 MK) $ (FK 1)
-      | S i'' =>
-          match i'' with
-          | O => (mask_34 MK) $ (FK 2)
-          | S i''' =>
-              match i''' with
-              | O => (mask_44 MK) $ (FK 3)
-              | S i'''' =>
-                  (K i'''' MK) $ (T' ((K i''' MK) $ (K i'' MK) $ (K i' MK) $ (CK (of_nat i''''))))
-              end
-          end
-      end
-  end.  
-
-Definition K_vec_aul (vec : @quadruple N)(i : nat) : @quadruple N :=
+Definition K_vec_aux (vec : @quadruple N)(i : nat) : @quadruple N :=
   ( (q2nd vec), (q3rd vec), (q4th vec), 
     (q1st vec) $ (T' ( (q2nd vec) $ (q3rd vec) $ (q4th vec) $ (CK (of_nat i) ) )) ). 
 
@@ -121,23 +53,16 @@ Fixpoint K_vec (i : nat) (MK : N) : @quadruple N :=
   match i with
   | O => ( (mask_14 MK) $ (FK 0), (mask_24 MK) $ (FK 1),
       (mask_34 MK) $ (FK 2), (mask_44 MK) $ (FK 3) )
-  | S i' => K_vec_aul (K_vec i' MK) i'
+  | S i' => K_vec_aux (K_vec i' MK) i'
   end. 
 
-Definition rk_ext  (MK : N) (i : nat): N := K (i + 4) MK. 
-Definition rk_ext_fast  (MK : N) (i : nat): N := q4th (K_vec (i + 1) MK). 
-
-
+Definition rk_ext (MK : N) (i : nat): N := q4th (K_vec (i + 1) MK). 
 
 Definition plain := to_N("0x0123456789abcdeffedcba9876543210"%string).
 Definition key := to_N("0x0123456789abcdeffedcba9876543210"%string).
 
-Check SM4_enc 32 plain (rk_ext key).  
-(* Runs forever *)
-(*Compute SM4_enc 32 plain (rk_ext key).  *)
-(* Compute of_N (SM4_enc 10 plain (rk_ext key)). cost about 40 secs. *)
-Definition cyphertext := of_N (SM4_enc_fast 32 plain (rk_ext_fast key)).  
-Definition decypheredtext := of_N (SM4_dec_fast 32 (to_N cyphertext) (rk_ext_fast key)). 
+Definition cyphertext := of_N (SM4_enc 32 plain (rk_ext key)).  
+Definition decypheredtext := of_N (SM4_dec 32 (to_N cyphertext) (rk_ext key)). 
 Definition expCypherText := "0x681edf34d206965e86b3e94f536e4246"%string. 
 Compute cyphertext. 
 Compute decypheredtext. 
