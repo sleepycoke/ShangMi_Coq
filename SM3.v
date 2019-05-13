92924f2d77f77da2f4ea54e66b3a2df3801cb5d0
sleepycoke <sleepycoke@users.noreply.github.com>
2019-05-13 17:29:42 +0800

    Update SM3.v

--- SM3.v
+++ SM3.v
@@ -23,25 +23,13 @@ Definition add_ws (m : N)(n : N) : N :=
 
 Infix "+ws" := add_ws (at level 50): N_scope. 
 
-Compute 9 /\ 10. 
-Compute of_N (~ 1 + 3). 
-Compute of_N (~ (1 + 3)). 
-Compute of_N ((~ 1) + 3). 
-
-Compute mask_ws. 
-
-Compute mask_ws +ws mask_ws. 
-
 Definition IV := 
   HexString.to_N("0x7380166f4914b2b9172442d7da8a0600a96f30bc163138aae38dee4db0fb0e4e").
-
-Compute IV. 
 
 (* 0 <= j <= 63*)
 Definition T (j : nat) : N := 
   if Nat.leb j 15 then HexString.to_N "0x79cc4519"  else HexString.to_N "0x7a879d8a". 
-Compute T 15. 
-Compute T 16. 
+
 (* 0 <= j <= 63, X Y Z are words*)
 Definition FF (j : nat) (X : N)(Y : N)(Z : N) : N :=
   if Nat.leb j 15 then X $ Y $ Z else
@@ -55,22 +43,8 @@ Definition GG (j : nat) (X : N)(Y : N)(Z : N) : N :=
 Definition P0 (X : N) := X $ (X <<< 9) $ (X <<< 17).
 Definition P1 (X : N) := X $ (X <<< 15) $ (X <<< 23).
 
-Compute modulo 13 5. 
-Compute Z.modulo (-13)%Z 5%Z. 
-
-Compute log2 3. 
-Compute size 31. 
-Compute size 0. 
-Compute to_N "0b11". 
-
 Definition pad_k (l : N) :=
   Z.to_N (Z.modulo (Z.sub 447%Z (Z.of_N l)) 512). 
-
-Compute pad_k 1.
-Compute pad_k 512.
-Compute pad_k 1024.
-
-Compute iter 3 (fun (s : string) => append s "1"%string) "01"%string.  
 
 Definition binaryStr (n : N) :=
   substring 2 (length (BinaryString.of_N n)) (BinaryString.of_N n). 
@@ -78,56 +52,16 @@ Definition binaryStr (n : N) :=
 Definition prePad64(s : string) :=
   append (iter (64 - (N.of_nat (length s))) (fun s => append s "0"%string) ""%string) s. 
 
-Compute binaryStr 3. 
-Compute binaryStr 0. 
-Compute binaryStr 24. 
-Compute length (binaryStr 24). 
-Compute prePad64 (binaryStr 24). 
-Compute BinaryString.of_N 0. 
-
 Definition Padding (m : string) (l : N) : string :=
   append ( iter (pad_k l) (fun (s : string) => append s "0"%string) (append m "1"%string)
   ) (prePad64 (binaryStr l)). 
 
 Compute Padding "011000010110001001100011" 24. 
 
-Compute shiftr 3 1.
-
 Definition n_of_B (l : N) := div (l + (pad_k l) + 65) 512. 
 
 Definition Block (i : nat)(m : string)(l : N) : N :=
   BinaryString.to_N ("0b" ++ (substring (i * 512) 512 (Padding m l))).
-
-Compute Block 0 "011000010110001001100011" 24. 
-Compute Block 1 "011000010110001001100011" 24. 
-Compute Block 2 "011000010110001001100011" 24. 
-Compute Block 0 "0" 1. 
-Compute Block 0 "1" 1. 
-
-Fixpoint W_slow (j : nat) (Bi : N) {struct j} : N :=
-  match (j - 15)%nat with 
-  (* j <= 15 *)
-  | O => ((shiftr Bi ((N.of_nat (Nat.sub 15 j)) * word_size)) /\ mask_ws)
-  | S j_16 (* j_16 = j - 16 *)=> 
-     match j with
-     | S (S (S j_3)) => 
-         match j_3 with
-         | S (S (S j_6)) =>
-             match j_6 with
-             | S (S (S j_9)) =>
-               match j_9 with
-               | S(S (S (S j_13))) => 
-                  P1 ( (W_slow j_16 Bi) $ (W_slow j_9 Bi) $ (W_slow j_3 Bi) <<< 15)
-                  $ (W_slow j_13 Bi) <<< 7 $ (W_slow j_6 Bi)
-               | _ => 0 (* impossible *)
-               end
-             | _ => 0 (* impossible *)
-             end
-         | _ => 0 (* impossible *)
-         end
-     | _ => 0 (* impossible *)
-     end
-  end.
 
 (* j <= 15 *)
 Fixpoint W_list_init (j : nat)(Bi : N) : list N :=
@@ -143,37 +77,14 @@ Definition W_list_aux (l' : list N) : list N :=
     $ (List.nth 12 l' 0) <<< 7 $ (List.nth 5 l' 0))
     :: l'. 
 
-Fixpoint W_list (j : nat)(Bi : N) {struct j}: list N :=
-  match (j - 15)%nat with
-  | O => W_list_init j Bi (* j <= 15 *)
-  | S j_16 => (* j_16 = j - 16 *)
-      match j with
-      | O => [] (* impossible *)
-      | S j' => W_list_aux (W_list j' Bi)
-      end
+Fixpoint W_list (j : nat)(l : list N) :=
+  match j with
+  | O => l
+  | S j' => W_list j' (W_list_aux l)
   end.
 
 Definition W (j : nat)(Bi : N) :=
-  List.nth 0 (W_list j Bi) 0. 
-
-(*
-Require Import FunInd.
-Require Import Recdef.
-Function W (j : N) (Bi : N) {measure j} : N :=
-  if N.leb j 15 then ((shiftr Bi (15 - j) * word_size) /\ mask_ws) else
-  P1 ( (W (j - 16) Bi) $ (W (j - 9) Bi) $ ((W (j - 3) Bi) <<< 15) $ ((W (j - 13) Bi) <<< 7) $ (W (j - 6) Bi) ).
-
-Definition W (j : nat) (Bi : N)  : N :=
-  (shiftr Bi (15 - (N.of_nat j)) * word_size) /\ mask_ws. 
-
-Definition Wv_aux (v : penta) :=  P1 ( (1st v)$ (2nd v) $ ((3rd v) <<< 15) 
-  $ ((4th v) <<< 7) $ (5th v).
-(* Wv_j = ( W_(j), W_(j + 7), W_(j + 13), W_(j + 3), W_(j + 10) ) , j <= 51*)
-Definition Wv (j : nat) (Bi : N) : N:= 
-  match j with
-  | 0 => ( (W 0 Bi), (W 7 Bi), (W 13 Bi), (W 3 Bi), (W 10 Bi) )
-  | S j' =>
-      *)
+  List.nth (67 - j)%nat (W_list 52 (W_list_init 15 Bi)) 0. 
 
 Definition Bitest := HexString.to_N "0x1111222233334444555566667777888899990000aaaabbbbccccddddeeeeffff1111222233334444555566667777888899990000aaaabbbbccccdd1deeeeffff". 
 Compute HexString.of_N (W 0 Bitest).  
@@ -185,7 +96,6 @@ Compute HexString.of_N (W 14 Bitest).
 Definition W' (j : nat) (Bi : N) :=
   (W j Bi) $ (W (j + 4) Bi). 
 
-Compute shiftr 1 1. 
 (* reg is 256 bit, i.e. 8 words. A is 1 word *)  
 Definition Part_A (reg : N) : N := (shiftr reg (7 * word_size)) /\ mask_ws. 
 Definition Part_B (reg : N) : N := (shiftr reg (6 * word_size)) /\ mask_ws. 
@@ -287,3 +197,4 @@ Compute HexString.of_N (Hash_hex exp_m). (* Correct *)
 Definition exp_m2 := "61626364616263646162636461626364616263646162636461626364616263646162636461626364616263646162636461626364616263646162636461626364".
 (*debe9ff9 2275b8a1 38604889 c18e5a4d 6fdb70e5 387e5765 293dcba3 9c0c5732*)
 Compute HexString.of_N (Hash_hex exp_m2). (* Correct *) 
+
