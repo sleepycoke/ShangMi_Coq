ca57e725314607b4437369453c21d4dd99a016e7
sleepycoke <sleepycoke@users.noreply.github.com>
2019-06-29 15:46:34 +0800

    Implemented 4.2.4 - 4.2.6

--- SM2.v
+++ SM2.v
@@ -1,14 +1,45 @@
 Require Import SMlib.
-
-
+Require Import Coq.Strings.Ascii.
 
 (* ByteList is indeed a list of bytes*)
 Definition BL := list byte. 
 (* BitList is indeed a list of bool*)
 Definition bL := list bool. 
+Fixpoint bS2bL_tail (bs : string)(acc : bL) : bL :=
+  match bs with
+  | EmptyString => acc 
+  | String head tl =>
+      match ascii_to_digit head with
+      | Some 1 => bS2bL_tail tl (List.app acc [true])
+      | _ => bS2bL_tail tl (List.app acc [false])
+      end
+  end.
 
-Fixpoint BL2N_tail (bl : BL)(acc : N) : N :=
+Definition bS2bL (bs : string) : bL :=
+  bS2bL_tail bs []. 
+
+Fixpoint bL2bS_tail (bl : bL)(acc : string) : string :=
   match bl with
+  | [] => acc
+  | head :: tl =>
+      match head with
+      | true => bL2bS_tail tl (acc ++ "1")
+      | false => bL2bS_tail tl (acc ++ "0")
+      end
+  end.
+
+Definition bL2bS (bl : bL) : string :=
+  bL2bS_tail bl "". 
+
+Compute bL2bS [true;false]. 
+
+Compute bS2bL "11001". 
+
+
+
+
+Fixpoint BL2N_tail (Bl : BL)(acc : N) : N :=
+  match Bl with
   | [] => acc
   | h :: tl =>
       BL2N_tail tl (acc * 256 + (Byte.to_N h)) 
@@ -17,8 +48,8 @@ Fixpoint BL2N_tail (bl : BL)(acc : N) : N :=
 Compute BL2N_tail [x01; x02] 0. 
 
 (*4.2.2*)
-Fixpoint BL2N (bl : BL) : N :=
-  BL2N_tail bl 0.
+Fixpoint BL2N (Bl : BL) : N :=
+  BL2N_tail Bl 0.
 
 Compute BL2N [x02; xaa]. 
 
@@ -63,16 +94,20 @@ Fixpoint bL2N_tail (bl : bL)(k : nat)(acc : N) : N :=
       end
   end.
 
-Definition bL2N (bl : bL)(k : nat) :=
+(*4.2.2*)
+Definition bL2N (bl : bL) :=
+  bL2N_tail bl (List.length bl) 0.
+
+Compute bL2N [true;true;true] . 
+
+Definition bL2N_prefix (bl :bL)(k : nat) : N :=
   bL2N_tail bl k 0.
 
-Compute bL2N [true;true;true] 4. 
-Compute bL2N [true;true;true] 3. 
-Compute bL2N [true;true;true] 2. 
+Compute bL2N_prefix [true;true;true] 2. 
 
 (* tranfrom the first k bits into a byte *)
 Definition bL2Byte (bl : bL)(k : nat) :=
-  N2Byte (bL2N bl k). 
+  N2Byte (bL2N_prefix bl k). 
 
 Compute bL2Byte [true;true;true] 4. 
 
@@ -119,3 +154,67 @@ Definition bL2BL (s : bL) : BL :=
 Compute bL2BL [true;true;true;false; true;true;true;true; true;true;true;true; true;true;true;true; true;true;false;true].  
 
 Compute bL2BL [true;true;true;true;true;true;true;true;false;true]. 
+
+Print byte. 
+
+Compute even 2. 
+
+Fixpoint N2bL_tail (n : N)(k : nat)(acc : bL) : bL :=
+  match k with
+  | O => acc
+  | S k' => 
+      N2bL_tail (N.div n 2) k' (N.odd n :: acc)
+  end.
+
+Compute N2bL_tail 254 8 []. 
+
+Definition N2bL (n : N) : bL :=
+  N2bL_tail n 8 [].
+
+Compute N2bL 127. 
+
+
+(*4.2.4*)
+Fixpoint BL2bL_tail (M : BL)(k : nat)(acc : bL) : bL :=
+  match k with
+  | O => acc
+  | S k'=> 
+      match M with
+      | [] => acc
+      | h :: tl =>
+          BL2bL_tail tl k' (List.app acc (N2bL (Byte.to_N h)))
+      end
+  end.
+
+Definition BL2bL (M : BL) : bL :=
+  BL2bL_tail M (List.length M) [].
+
+Compute BL2bL [].
+Compute BL2bL [xff].
+Compute BL2bL [x01; xff].
+
+(*4.2.5*)
+Definition Field2BL_a (alpha : N) :=
+  N2BL alpha. 
+
+Definition Field2BL_b (alpha : bL) :=
+  bL2BL alpha. 
+
+(*4.2.6*)
+Definition BL2Field_a (Bl : BL)(q : N) : option N :=
+  (fun (alpha : N)  => if leb q alpha then None else Some alpha) (BL2N Bl).  
+
+Definition BL2Field_b (Bl : BL) : bL :=
+  BL2bL Bl. 
+
+Compute BL2Field_a [x07] 7. 
+Compute BL2Field_a [x06] 7. 
+
+(*4.2.7*)
+Definition Field2N_b (alpha : bL) : N :=
+  bL2N alpha. 
+
+(*4.2.8*)
+
+
+
