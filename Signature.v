511324c08870a0f3f848a876c49e08f9d3afacfe
sleepycoke <sleepycoke@users.noreply.github.com>
2019-09-05 15:26:18 +0800

    Struggled for quite a while but still don't get ENTL.

---
+++ Signature.v
@@ -0,0 +1,32 @@
+Require Export SM2_SysPara.
+
+Record aaa := mkAAA {abb : nat; bbb : bool}.
+Definition aba := {|abb := 1; bbb := true|}. 
+Compute aba.
+Compute aba.(abb). 
+
+Definition abc := mkAAA 2 false. 
+
+Compute abc.
+Compute bbb abc. 
+Compute abc.(abb).  
+
+Print N2bL. 
+
+(*5.5 trunk from right *)
+Definition ENTL (ID : N) :=
+  N2bL_len (N.of_nat (List.length (N2BL ID))) 16. 
+
+Print Datatypes.length. 
+
+Open Scope list. 
+Definition Z_A (ID_A a b xG yG xA yA : N) :=
+  (ENTL ID_A) ++ (N2bL a) ++ (N2bL b) 
+    ++ (N2bL xG) ++ (N2bL yG) ++ (N2bL xA) ++ (N2bL yA). 
+
+Compute hS2N "A b".
+
+Definition IDa := hS2N "414C 49434531 32334059 41484F4F 2E434F4D".
+Compute IDa. 
+Compute List.length (N2BL IDa). 
+Compute bL2hS (ENTL IDa).  (*wrong*)
