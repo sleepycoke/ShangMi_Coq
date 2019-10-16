21bf04e715bae934d96d1a303b55af2152913cd8
sleepycoke <sleepycoke@users.noreply.github.com>
2019-10-16 11:57:20 +0800

    Tried to speed up F_sqr but fruitless

--- SM2_SysPara.v
+++ SM2_SysPara.v
@@ -146,6 +146,36 @@ Definition pf_eqb (P1 P2 : FEp) : bool :=
       andb (x1 =? x2) (y1 =? y2)
   end.
 
+Print Pos.square. 
+Check N.modulo. 
+Check Pos.add.
+Open Scope positive_scope. 
+Fixpoint F_sqr_pos (n q : positive) : N :=
+  match n with
+  | 1 => 1
+  | p~1 => 
+      match ((F_sqr_pos p q) + (Npos p)) mod (Npos q) with
+      | N0 => 1
+      | Npos m => (Npos m~0~1) mod (Npos q)
+      end
+  | p~0 => 
+      match (F_sqr_pos p q) with
+      | N0 => 0
+      | Npos m => (Npos m~0) mod (Npos q)
+      end
+  end. 
+Close Scope positive_scope. 
+ 
+Definition F_sqr (n q : N) :=
+  match q with
+  | 0 => n 
+  | Npos q' => 
+    match n with
+    | 0 => 0
+    | Npos n' =>
+        F_sqr_pos n' q'
+    end
+  end. 
 (* 3.2.3.1 also A.1.2.2 *)
 Definition pf_double (P1 : FEp)(p : N)(a : N) :=
   match P1 with
@@ -156,6 +186,36 @@ Definition pf_double (P1 : FEp)(p : N)(a : N) :=
       let y3 := F_sub (lambda * (F_sub x1 x3 p)) y1 p in
         Cop (x3, y3)
   end. 
+Definition pf_double_mul (P1 : FEp)(p : N)(a : N) :=
+  match P1 with
+  | InfO => InfO
+  | Cop (x1, y1) =>
+      let lambda := F_div (3 * (x1 * x1) + a) (double y1) p in
+      let x3 := F_sub (lambda * lambda) ((double x1) mod p) p in
+      let y3 := F_sub (lambda * (F_sub x1 x3 p)) y1 p in
+        Cop (x3, y3)
+  end. 
+Definition pf_double_sqr (P1 : FEp)(p : N)(a : N) :=
+  match P1 with
+  | InfO => InfO
+  | Cop (x1, y1) =>
+      let lambda := F_div (3 * (F_sqr x1 p) + a) (double y1) p in
+      let x3 := F_sub (F_sqr lambda p) ((double x1) mod p) p in
+      let y3 := F_sub (lambda * (F_sub x1 x3 p)) y1 p in
+        Cop (x3, y3)
+  end. 
+
+Locate hS2N. 
+Definition p := hS2N "8542D69E 4C044F18 E8B92435 BF6FF7DE 45728391 5C45517D 722EDB8B 08F1DFC3". 
+Definition a := hS2N "787968B4 FA32C3FD 2417842E 73BBFEFF 2F3C848B 6831D7E0 EC65228B 3937E498". 
+Definition x2 := hS2N "64D20D27 D0632957 F8028C1E 024F6B02 EDF23102 A566C932 AE8BD613 A8E865FE".
+Definition y2 := hS2N "64D20D27 D0632957 F8028C1E 024F6B02 EDF23102 A566C932 AE8BD613 A8E865FE".
+Definition P2 := Cop (x2, y2). 
+Time Compute pf_double_sqr P2 p 2.  (* 25 68 1.9s *)
+Time Compute pf_double P2 p 2. (* 1.681 secs originally *)
+Time Compute pf_double_mul P2 p 2. (* 1.607 secs *)
+Locate Pos.square. 
+Print Pos.square. 
 
 Definition pf_add (P1 P2 : FEp)(p a : N):=
   match P1, P2 with
