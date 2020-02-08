828d488fcd590eded4b0a4a064cd783342daaac5
sleepycoke <sleepycoke@users.noreply.github.com>
2020-02-09 00:28:17 +0800

    Finished bfp

--- PubKey.v
+++ PubKey.v
@@ -2,7 +2,7 @@ Require Export KeyEx.
 
 (*A2 - A5 *)
 Definition TryComputeTwithK (hash_v : bL -> bL)(v : nat)(klen : nat)
-  (ml : GE -> N -> GE)(p2Bl : N -> N -> BL)(k h : N)(G PB : GE)
+  (ml : GE -> N -> GE)(p2Bl : N -> N -> BL)(n2bl : N -> bL)(k h : N)(G PB : GE)
   : optErr (option (bL * bL * bL * bL)) :=
   let C1 := ml G k in
   match C1 with
@@ -17,7 +17,7 @@ Definition TryComputeTwithK (hash_v : bL -> bL)(v : nat)(klen : nat)
         match kPB with
         | InfO => Error "kPB = InfO" (* impossible? *)
         | Cop (x2, y2) => 
-          let (x2bl, y2bl) := (N2BbL x2, N2BbL y2) in
+          let (x2bl, y2bl) := (n2bl x2, n2bl y2) in
           let t := KDF (x2bl ++ y2bl) klen hash_v v in
            if All0bL t then Normal None
            else Normal (Some (t, x2bl, y2bl, C1bl))
@@ -26,15 +26,15 @@ Definition TryComputeTwithK (hash_v : bL -> bL)(v : nat)(klen : nat)
   end. 
 
 (* A1 - A8 *)
-Fixpoint ComputeCwithklist (hash_v : bL -> bL)(v : nat)(ml : GE -> N -> GE)(p2Bl : N -> N -> BL)(h : N)(klist : list N)
+Fixpoint ComputeCwithklist (hash_v : bL -> bL)(v : nat)(ml : GE -> N -> GE)(p2Bl : N -> N -> BL)(n2bl : N -> bL)(h : N)(klist : list N)
   (G PB : GE)(M : bL) : optErr bL :=
   match klist with
   | [] => Error "klist depleted"
   | k :: tl =>
-      match TryComputeTwithK hash_v v (length M) ml p2Bl k h G PB  with
+      match TryComputeTwithK hash_v v (length M) ml p2Bl n2bl k h G PB  with
       | Error err => Error (err ++ " k = " ++ (N2hS k))
       | Normal None =>
-          ComputeCwithklist hash_v v ml p2Bl h tl G PB M
+          ComputeCwithklist hash_v v ml p2Bl n2bl h tl G PB M
       | Normal (Some (t, x2bl, y2bl, C1bl)) =>
           let C2 := bLXOR M t in
           let C3 := hash_v (x2bl ++ M ++ y2bl) in
@@ -43,13 +43,13 @@ Fixpoint ComputeCwithklist (hash_v : bL -> bL)(v : nat)(ml : GE -> N -> GE)(p2Bl
   end. 
 
 Definition ComputeCwithklist_pf (hash_v : bL -> bL)(v : nat)(p a h : N)(klist : list N)(cp : cmp_type)(G PB : GE)(M : bL) : optErr bL :=
-  ComputeCwithklist hash_v v (pf_mul p a) (Point2BL_p cp) h klist G PB M. 
+  ComputeCwithklist hash_v v (pf_mul p a) (Point2BL_p cp) N2BbL h klist G PB M. 
 
-Definition ComputeCwithklist_bpf (hash_v : bL -> bL)(v : nat)(m gp a h : N)(klist : list N)(cp : cmp_type)(G PB : GE)(M : bL) : optErr bL :=
-  ComputeCwithklist hash_v v (bfp_mul m gp a) (Point2BL_b m cp) h klist G PB M. 
+Definition ComputeCwithklist_bfp (hash_v : bL -> bL)(v : nat)(m gp a h : N)(klist : list N)(cp : cmp_type)(G PB : GE)(M : bL) : optErr bL :=
+  ComputeCwithklist hash_v v (bfp_mul m gp a) (Point2BL_b m cp) (N2BbL_len (N.to_nat m)) h klist G PB M. 
 
 (* B1 - B7 *)
-Definition ComputeM' (hash_v : bL -> bL)(v : nat)(klen : nat)(ml : GE -> N -> GE)(OnCrv : N -> N -> bool)(Bl2p : BL -> option (N * N))(h dB : N)(C : bL) : optErr bL :=
+Definition ComputeM' (hash_v : bL -> bL)(v : nat)(klen : nat)(ml : GE -> N -> GE)(OnCrv : N -> N -> bool)(Bl2p : BL -> option (N * N))(n2bl : N -> bL)(h dB : N)(C : bL) : optErr bL :=
   let (C1bl, C2C3) := partListBack C (Nat.add klen v) in
   match Bl2p (bL2BL C1bl) with
   | None => Error "Failed to uncompress C1"
@@ -64,7 +64,7 @@ Definition ComputeM' (hash_v : bL -> bL)(v : nat)(klen : nat)(ml : GE -> N -> GE
         match dBC1 with
         | InfO => Error "dBC1 = InfO"
         | Cop (x2, y2) =>
-            let (x2bl, y2bl) := (N2BbL x2, N2BbL y2) in
+            let (x2bl, y2bl) := (n2bl x2, n2bl y2) in
             let t := KDF (x2bl ++ y2bl) klen hash_v v in
             if All0bL t then Error "t is all 0s" else
             let (C2, C3) := partList C2C3 klen in
@@ -76,15 +76,11 @@ Definition ComputeM' (hash_v : bL -> bL)(v : nat)(klen : nat)(ml : GE -> N -> GE
     end
   end. 
 
-Print bfp_mul. 
-Print OnCurve_bfp. 
-Print BL2Point_bfp. 
-
 Definition ComputeM'_pf (hash_v : bL -> bL)(v : nat)(klen : nat)(p a b h dB : N)(cp : cmp_type)(C : bL) : optErr bL :=
-  ComputeM' hash_v v klen (pf_mul p a) (OnCurve_pf p a b) (BL2Point_p cp p a b) h dB C . 
+  ComputeM' hash_v v klen (pf_mul p a) (OnCurve_pf p a b) (BL2Point_p cp p a b) N2BbL h dB C . 
 
 Definition ComputeM'_bfp (hash_v : bL -> bL)(v : nat)(klen : nat)(m gp a b h dB : N)(cp : cmp_type)(C : bL) : optErr bL :=
-  ComputeM' hash_v v klen (bfp_mul m gp a) (OnCurve_bfp gp a b) (BL2Point_bfp cp m gp a b) h dB C . 
+  ComputeM' hash_v v klen (bfp_mul m gp a) (OnCurve_bfp gp a b) (BL2Point_bfp cp m gp a b) (N2BbL_len (N.to_nat m)) h dB C . 
 
 Module test_pf. 
 Definition p := hS2N "8542D69E 4C044F18 E8B92435 BF6FF7DE 45728391 5C45517D 722EDB8B 08F1DFC3".
@@ -153,6 +149,70 @@ Correct
 End test_pf. 
 
 Module test_bfp.
+Definition m := 257%N.
+Definition gp := (N.shiftl 1 257) + (N.shiftl 1 12) + 1. 
+Definition a := 0%N.
+Definition b := hS2N "00 E78BCD09 746C2023 78A7E72B 12BCE002 66B9627E CB0B5A25 367AD1AD 4CC6242B".
+Definition h := 4%N. 
+Definition xG := hS2N "00 CDB9CA7F 1E6B0441 F658343F 4B10297C 0EF9B649 1082400A 62E7A748 5735FADD".
+Definition yG := hS2N "01 3DE74DA6 5951C4D7 6DC89220 D5F7777A 611B1C38 BAE260B1 75951DC8 060C2B3E".
+Definition G := Cop (xG, yG). 
+Definition n := hS2N "7FFFFFFF FFFFFFFF FFFFFFFF FFFFFFFF BC972CF7 E6B6F900 945B3C6A 0CF6161D". 
+Definition M := hS2bL "656E63 72797074 696F6E20 7374616E 64617264".
+Definition klen := 152%nat. 
+Definition dB := hS2N "56A270D1 7377AA9A 367CFA82 E46FA526 7713A9B9 1101D077 7B07FCE0 18C757EB".
+Definition xB := hS2N "00 A67941E6 DE8A6180 5F7BCFF0 985BB3BE D986F1C2 97E4D888 0D82B821 C624EE57". 
+Definition yB := hS2N "01 93ED5A67 07B59087 81B86084 1085F52E EFA7FE32 9A5C8118 43533A87 4D027271". 
+Definition PB := Cop (xB, yB). 
+Definition k := hS2N "6D3B4971 53E3E925 24E5C122 682DBDC8 705062E2 0B917A5F 8FCDB8EE 4C66663D". 
+Definition x1 := hS2N "01 9D236DDB 305009AD 52C51BB9 32709BD5 34D476FB B7B0DF95 42A8A4D8 90A3F2E1". 
+Definition y1 := hS2N "00 B23B938D C0A94D1D F8F42CF4 5D2D6601 BF638C3D 7DE75A29 F02AFB7E 45E91771". 
 
+Definition x2 := hS2N "00 83E628CF 701EE314 1E8873FE 55936ADF 24963F5D C9C64805 66C80F8A 1D8CC51B".
+Definition y2 := hS2N "01 524C647F 0C0412DE FD468BDA 3AE0E5A8 0FCC8F5C 990FEE11 60292923 2DCD9F36". 
+Definition P2 := Cop (x2, y2). 
+Print KDF. 
+Definition t := KDF ((N2BbL_len 257 x2) ++ (N2BbL_len 257 y2)) klen Hash constant_v. 
+(*Compute bL2hS t.*) (*Correct*)
+Definition C2 := bLXOR M t . 
+(*Compute bL2hS C2.*) (*Correct*)
+Definition C3 := Hash ((N2BbL_len 257 x2) ++ M ++ (N2BbL_len 257 y2)). 
+(*Compute bL2hS C3. *)(*Correct*) 
+(*
+Time Compute match ComputeCwithklist_bfp Hash constant_v m gp a h [k] ucp G PB M with
+| Error s => Error s
+| Normal bl => Normal (bL2hS bl)
+end. 
+*)
+(*
+= Normal
+         "04019d236ddb305009ad52c51bb932709bd534d476fbb7b0df9542a8a4d890a3f2e1b23b938dc0a94d1df8f42cf45d2d6601bf638c3d7de75a29f02afb7e45e91771fd55ac6213c2a8a040e4cab5b26a9cfcda737373a48625d3758fa37b3eab80e9cfcaba665e3199ea15a1fa8189d96f579125e4"
+     : optErr string
+Finished transaction in 2365.322 secs (2340.379u,5.914s) (successful)
+x1 C2 C3 are correcet, C2 missing leading 00
+*)
+Definition C1C2C3 := hS2bL "04019d236ddb305009ad52c51bb932709bd534d476fbb7b0df9542a8a4d890a3f2e100b23b938dc0a94d1df8f42cf45d2d6601bf638c3d7de75a29f02afb7e45e91771fd55ac6213c2a8a040e4cab5b26a9cfcda737373a48625d3758fa37b3eab80e9cfcaba665e3199ea15a1fa8189d96f579125e4". 
+
+Definition C := hS2bL "04019D23 6DDB3050 09AD52C5 1BB93270 9BD534D4 76FBB7B0 DF9542A8 A4D890A3 F2E100B2 3B938DC0 A94D1DF8 F42CF45D 2D6601BF 638C3D7D E75A29F0 2AFB7E45 E91771FD 55AC6213 C2A8A040 E4CAB5B2 6A9CFCDA 737373A4 8625D375 8FA37B3E AB80E9CF CABA665E 3199EA15 A1FA8189 D96F5791 25E4". 
+(*
+Compute bLXOR C1C2C3 C. 
+Compute List.length C1C2C3.
+Compute List.length C. 
+Correct *)
+(*
+Time Compute match ComputeM'_bfp Hash constant_v klen m gp a b h dB ucp C with
+| Error s => Error s
+| Normal bl => Normal (bL2hS bl)
+end. 
+*)
+(*
+= Normal "656e6372797074696f6e207374616e64617264"
+     : optErr string
+*)
+(*
+Definition M' := hS2bL "656e6372797074696f6e207374616e64617264".
+Compute bLXOR M  M'. 
+*)
+(*Correct *)
 End test_bfp.
 
